09ad5877edcab803481675fc0e5d66bc6188ca7c
Daniel Nguyen <dtnguyen63@gmail.com>
2011-07-21 16:36:27 -0500

    Done with Exercise 3.5.1

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -3,6 +3,10 @@ require 'spec_helper'
 describe PagesController do
   render_views
   
+  before(:each) do
+  @base_title = "Ruby on Rails Tutorial Sample App | "
+  end
+  
   describe "GET 'home'" do
     it "should be successful" do
       get 'home'
@@ -11,7 +15,7 @@ describe PagesController do
 
     it "should have the right title" do
       get 'home'
-      response.should have_selector("title", :content => "Ruby on Rails Tutorial Sample App | Home")
+      response.should have_selector("title", :content => @base_title + "Home")
     end
   end
 
