db5e6f1d4e76fb2dec0f8aa45e5c243109db4058
Daniel Nguyen <dtnguyen63@gmail.com>
2011-07-21 16:39:42 -0500

    Done with Exercise 3.5.2

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -27,7 +27,7 @@ describe PagesController do
     
     it "should have the right title" do
       get 'contact'
-      response.should have_selector("title", :content => "Ruby on Rails Tutorial Sample App | Contact")
+      response.should have_selector("title", :content => @base_title + "Contact")
     end
   end
   
@@ -39,7 +39,7 @@ describe PagesController do
     
     it "should have the right title" do
       get 'about'
-      response.should have_selector("title", :content => "Ruby on Rails Tutorial Sample App | About")
+      response.should have_selector("title", :content => @base_title + "About")
     end
   end
   
@@ -51,7 +51,7 @@ describe PagesController do
     
     it "should have the right title" do
       get 'help'
-      response.should have_selector("title", :content => "Ruby on Rails Tutorial Sample App | Help")
+      response.should have_selector("title", :content => @base_title + "Help")
     end
   end
 
