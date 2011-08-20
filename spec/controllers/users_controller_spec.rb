8d47113c5f069d482150199e9c4be3fcb297e6f6
Daniel Nguyen <dtnguyen63@gmail.com>
2011-08-19 19:04:33 -0500

    finished chapter 8.6.1-8.6.3

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -45,6 +45,26 @@ describe UsersController do
 		get :new
 		response.should have_selector("title", :content => "Sign up")
 	end
+	
+	it "should have a name field" do
+		get :new
+		response.should have_selector("input[name='user[name]'][type='text']")
+	end
+	
+	it "should have an email field" do
+		get :new
+		response.should have_selector("input[name='user[email]'][type='text']")
+	end
+	
+	it "should have a password field" do
+		get :new
+		response.should have_selector("input[name='user[password]'][type='password']")
+	end
+	
+	it "should have a password confirmation field" do
+		get :new
+		response.should have_selector("input[name='user[password_confirmation]'][type='password']")
+	end
   end
   
   describe "POST 'create'" do
@@ -52,7 +72,7 @@ describe UsersController do
   	describe "failure" do
   		
   		before(:each) do
-  			@attr = { :name => "", :email => "", :password => "", :password_confirmation => ""}
+  			@attr = { :name => "", :email => "", :password => "foo", :password_confirmation => ""}
   		end
   		
   		it "should not create a user" do
@@ -69,6 +89,11 @@ describe UsersController do
   		it "should render the 'new' page" do
   			post :create, :user => @attr
   			response.should render_template('new')
+  		end
+  		
+  		it "should clear the password field" do
+  			post :create, :user => @attr
+  			response.should have_selector("input[name='user[password]'][value='']")
   		end
   	end
   	
